6811c88da06dc067a25794637b3dfc05465f2174
Pedro Vieira <86010036+pedrovs3@users.noreply.github.com>
2021-11-04 22:43:09 -0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ var html = `
       <div class="content">
         <span class="stars"></span>
         <h2 class="java">DICA 1</h2>
-        <p class="java">Primeiro, se você acha que precisa diminuir o tempo que você joga, é importante organizar o seu tempo, caso você faça as mesmas coisas todos os dias, uma organização baseada em rotinas seria uma boa opção, além de ser mais produtivo com o seu tempo, objetivos e metas, dá pra organizar melhor o seu tempo de lazer que poderia ser para sua jogatina e apps como o calendário da google podem ajudar nessa organização.</p>
+        <p class="java">Se você acha que precisa diminuir o tempo que você joga, é importante organizar o seu tempo, uma organização baseada em rotinas seria uma boa opção, além de ser mais produtivo com o seu tempo, objetivos e metas, dá pra organizar melhor o seu tempo de lazer!</p>
       </div>
     </div>
     <div class="face face2">
@@ -19,7 +19,7 @@ var html = `
       <div class="content">
         <span class="stars"></span>
         <h2 class="python">DICA 2</h2>
-        <p class="python">comece a mudar sua rotina de jogo e se programe para que a cada 2 horas você levante da cadeira, beba uma água e se possível, faça alguns alongamentos, seguindo essas dicas, você se tornará uma pessoa mais saudável sem a necessidade de abandonar suas partidas. Bom Game!</p>
+        <p class="python">Comece a mudar sua rotina de jogo e se programe para que a cada 2 horas você levante da cadeira, beba uma água e se possível, faça alguns alongamentos, seguindo essas dicas, você se tornará uma pessoa mais saudável sem a necessidade de abandonar suas partidas. Bom Game!</p>
       </div>
     </div>
     <div class="face face2">
@@ -32,7 +32,7 @@ var html = `
       <div class="content">
         <span class="stars"></span>
         <h2 class="cSharp">DICA 3</h2>
-        <p class="cSharp">Se estiver procurando alguma ajuda para se estressar menos com jogos, esse é um tópico difícil pois depende muito do seu tipo de jogo, por exemplo, em jogos online é mais difícil por você estar em volta de outras pessoas, é mais fácil se estressar quando se está em uma comunidade tóxica, caso queira se estressar menos opte por jogos singleplayer, que o seu estresse não dependa de outras pessoas, e também jogos mais relaxantes, que não sejam difíceis.</p>
+        <p class="cSharp">Os exercícios ajudam e muito a melhorar o humor bastando cerca de cinco minutos para alegrá-lo, com a elevação dos batimentos o organismo tende a liberar endorfinas que o te façam sentir melhor!</p>
       </div>
     </div>
     <div class="face face2">
@@ -44,7 +44,7 @@ var html = `
       <div class="content">
         <span class="stars"></span>
         <h2 class="python">DICA 4</h2>
-        <p class="python">Ficar sentado por muito tempo faz com que uma parte do sangue do sangue que está circulando pelo seu corpo fique concentrado da cintura para baixo, assim, quando você se levantar depois de uma longa jogatina, terá pressão baixa, alta taxa de desenvolver varizes e labirintite. Para resolver este problema, é necessário que você estenda suas pernas para frente ainda sentado antes de levantar, e mantenha-a por 10 segundos, assim, o sangue que estava “preso” abaixo da cintura terá o tempo necessário para voltar a sua circulação normalmente.</p>
+        <p class="python">Faça uma pausa. Quando estiver nervoso, distraia-se dos aspectos irritantes do jogo fazendo alguma outra coisa por um tempo. Separar um momento para descansar pode fazer com que volte ao jogo com a cabeça fresca e tenha sucesso!!</p>
       </div>
     </div>
     <div class="face face2">
